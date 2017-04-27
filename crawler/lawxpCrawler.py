a0227d986272d79df68b266e85a88e6720aa55be
Administrator <bun@csip.org.cn>
2017-04-27 17:46:54 +0800

    更新

--- crawler/lawxpCrawler.py
+++ crawler/lawxpCrawler.py
@@ -2,6 +2,11 @@ import requests
 from bs4 import BeautifulSoup
 
 import urllib
+
+from util import mongoUtil
+
+coll = mongoUtil.getCollection('lawxy')
+
 #借助site:gov.cn  来寻找政府网址。
 def getLawxpData(RegionId,keyword,page):
     param = {'RegionId':RegionId,'q':keyword,'pg':page}
@@ -13,6 +18,7 @@ def getLawxpData(RegionId,keyword,page):
     # print(page_content)
     liList = page_content.find_all('li')
     print('+++++++++++++++++++++++++++++++++++++++++++++++++++++++++')
+
     for li in liList:
         #文件标题
         title= li.find('div','zyin-ll-bt1').a.text
@@ -20,7 +26,7 @@ def getLawxpData(RegionId,keyword,page):
         publishId = li.find('span','zyin-ll-bt2').text.replace('发布字号：','')
         #发布机构
         publishOffice = li.find('div','fleft zyin-fbjg').text.replace('发布机构：','')
-        publishTime = li.find('div','zyin-ll-rq fright').text[5:16]
+        publishTime = li.find('div','zyin-ll-rq fright').text[5:15]
         print('Id:',publishId)
         print('-----------------------------------------')
         print('time:',publishTime)
@@ -30,19 +36,22 @@ def getLawxpData(RegionId,keyword,page):
         # print(li)
         print('office:',publishOffice)
         print('----------------------------------------------------------')
-
-
-# import urllib2
-# import urllib
-# def baidu_search(keyword,pn):
-#     p= {'wd': keyword}
-#     res=urllib2.urlopen(("http://www.baidu.com/s?"+urllib.urlencode(p)+"&pn={0}&cl=3&rn=100").format(pn))
-#     html=res.read()
-#     return html
+        doc={'文件标题':title,'发布字号':publishId,'发布机构':publishOffice,'发布时间':publishTime}
+        coll.insert(doc)
 
 
 if __name__=='__main__':
-    RegionId='2'
+    #2:北京，
+    #100000:中央
+    #3:天津
+    #4:河北
+    #6:内蒙古
+    #5:山西
+    #16:山东
+    #
+    RegionId='100000'
     keyword='大气'
     page=1
-    getLawxpData(RegionId,keyword,page)
+    size=6
+    for i in range(1,size+1):
+        getLawxpData(RegionId,keyword,i)
